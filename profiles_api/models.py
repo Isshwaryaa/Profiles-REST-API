047bad693799213e796e61bbf9cd062586bd89e0
Isha <isshwaryaas@gmail.com>
2022-02-12 20:42:05 -0500

    Enabled dajango admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -8,7 +8,10 @@ class UserProfileManager(BaseUserManager):
     def create_user(self, email, name, password=None):
         if not email:
             raise ValueError('User must have an email address')
-        emai = self.normalize_email(email)
+        if not name:
+            raise ValueError("User must have a full name")
+        email = self.normalize_email(email)
+        
         user = self.model(email=email, name=name)
 
         user.set_password(password)
@@ -17,6 +20,7 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password):
         user = self.create_user(email, name, password)
+
         user.is_superuser = True
         user.is_staff = True
 
@@ -26,15 +30,15 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, default='Isha')
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
 
-    Objects = UserProfileManager()
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FILELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         return self.name
